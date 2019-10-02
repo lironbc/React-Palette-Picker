c20414e0a7e62781890c035ee96757f41b6810d3
lironbc <36120346+lironbc@users.noreply.github.com>
2019-10-02 00:57:04 -0700

    Updates

---
+++ precache-manifest.e0812adda536dca79b9cbd621cff60a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7791601696a939ecbfb9d881d9ba3447",
+    "url": "/React-Palette-Picker/index.html"
+  },
+  {
+    "revision": "47bc9969f80867ec82f7",
+    "url": "/React-Palette-Picker/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "03b4e9d0c249400de7c7",
+    "url": "/React-Palette-Picker/static/css/main.6926d412.chunk.css"
+  },
+  {
+    "revision": "47bc9969f80867ec82f7",
+    "url": "/React-Palette-Picker/static/js/2.b5d4a8b5.chunk.js"
+  },
+  {
+    "revision": "03b4e9d0c249400de7c7",
+    "url": "/React-Palette-Picker/static/js/main.bfe9b589.chunk.js"
+  },
+  {
+    "revision": "2ea30ddd4790b683d571",
+    "url": "/React-Palette-Picker/static/js/runtime~main.ce171d02.js"
+  },
+  {
+    "revision": "abc613a56000e1941faed84f47cf3383",
+    "url": "/React-Palette-Picker/static/media/bg.abc613a5.svg"
+  }
+]);
